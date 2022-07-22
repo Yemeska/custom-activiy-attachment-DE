d123d01fdc8b5561d01157faea908980c9819093
Mihail Hristov <mihail.hristov@Admins-MacBook-Air.local>
2022-07-22 11:01:50 +0300

    small changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var errorhandler = require('errorhandler');
-var http        = require('http');
+var https        = require('https');
 var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
@@ -37,6 +37,6 @@ app.post('/journeybuilder/validate/', activity.validate);
 app.post('/journeybuilder/publish/', activity.publish);
 app.post('/journeybuilder/execute/', activity.execute);
 
-http.createServer(app).listen(app.get('port'), function(){
+https.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
