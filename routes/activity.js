db3b4ce348dbbb61f83c43bb0daa41d52de12808
Mihail Hristov <mihail.hristov@Admins-MacBook-Air.local>
2022-08-19 15:21:11 +0300

    small changes

--- routes/activity.js
+++ routes/activity.js
@@ -435,7 +435,7 @@ function httpRequest( optionsParam, postData ) {
 }
 
 
-const job = schedule.scheduleJob('00 14 12 * * 0-6', function(){
+const job = schedule.scheduleJob('00 23 12 * * 0-6', function(){
     console.log('running a task to deliting assests!');
 
     var MC_BODY_OAUTH2 = JSON.stringify({
